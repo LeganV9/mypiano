644fdefc6bd843878a274e1b3194bb60844a4171
LeganV9 <yanisrenaud77@gmail.com>
2023-02-24 02:23:16 +0100

    upload first files

---
+++ script.js
@@ -0,0 +1,13 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("tunes/q.wav"); // by default, audio src is "q" tune
+
+const playTune = (key) = {
+    audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed
+    audio.play(); // playing audio
+}
+
+pianoKeys.forEach(key => {
+    // calling playtune function with passing data-key value as an argument
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
